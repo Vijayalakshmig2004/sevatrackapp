66a4f2f2874c6d34774e7e1aaf6cde27b5ec4757
vijayalakshmicg2004 <vijayalakshmicg2004@gmail.com>
2026-06-19 14:13:09 +0530

    Configure Next.js for static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isExport = process.env.NEXT_PUBLIC_EXPORT === 'true';
-
 const nextConfig = {
-  output: isExport ? 'export' : undefined,
-  basePath: isExport ? '/sevatrackapp' : undefined,
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-};
-
-if (!isExport) {
-  nextConfig.headers = async () => {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, 
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ];
-  };
 }
 
-export default nextConfig;
+export default nextConfig
